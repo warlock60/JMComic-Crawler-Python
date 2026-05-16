0bd6f1e734e3590cb602324835cd96859f43beb9
hect0x7 <93357912+hect0x7@users.noreply.github.com>
2026-05-16 22:52:19 +0800

    v2.6.20: 同步禁漫html正则表达式

--- src/jmcomic/__init__.py
+++ src/jmcomic/__init__.py
@@ -2,7 +2,7 @@
 # 被依赖方 <--- 使用方
 # config <--- entity <--- toolkit <--- client <--- option <--- downloader
 
-__version__ = '2.6.19'
+__version__ = '2.6.20'
 
 from .api import *
 from .jm_plugin import *
